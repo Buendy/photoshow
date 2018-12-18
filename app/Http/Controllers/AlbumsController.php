3c1d353388bd21907aa773f7c9234c649cad0c1e
Daniel <danielbuendiasmr@gmail.com>
2018-12-18 16:05:49 +0000

    Fin parte 3

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -16,8 +16,25 @@ class AlbumsController extends Controller
         return view('albums.create');
     }
 
-    public function store()
+    public function store(Request $request)
     {
+            //Realizamos validaciones (SE PUEDEN Y SE DEBE AÑADIR MAS VALIDACIONES)
+        $this->validate($request, [
+           'name' => 'required',  //El campo ha de ser requerido
+           'cover_image' => 'image|max:2000', //Se debe de subir una imagen de como máximo 2000KB
 
+        ]);
+            //EL nombre del fichero que ha subido el usuario (tiene el nombre que tenía en su equipo)
+        $fileNameWithExt = $request->file('cover_image')->getClientOriginalName();
+            //pathinfo() es un helper de laravel
+        $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+            //Obtenemos la extensión original con la que el usuario ha subido el fichero
+        $extension = $request->file('cover_image')->getClientOriginalExtension();
+            //La manera de de guardar el archivo (nombre, fechaSubida, extensión)
+        $fileNameToStore = $fileName . '_' . time() . '.' . $extension;
+            //Ruta donde se guardarán las imagenes, y el archivo a subir
+        $path = $request->file('cover_image')->storeAs('public/albums_covers', $fileNameToStore);
+
+        return $path;
     }
 }
